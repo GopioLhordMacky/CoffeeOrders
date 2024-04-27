3e1de3b8f080d1ff8a638ee47f6042a06b4fd355
GopioLhordMacky <165142352+GopioLhordMacky@users.noreply.github.com>
2024-04-27 08:48:52 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Arrays
+{
+    class ArrayReview
+    {
+        static void Main(string[] args)
+        {
+            string[] coffee = { "Mocha", "Latte", "Matcha", "Americano" };
+            int num;
+            Console.Write("Enter Coffee Code: ");
+            num = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("You ordered: " + coffee[num]);
+            
+        }
+    }
+}
